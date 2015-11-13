2128b1d53404a261f6327d57e95b84f402d67c52
DanielW093 <Danielw093@gmail.com>
2015-11-13 23:56:26 +0000

    Commit models

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed;
+	CharacterController controller;
+
+	const float gravity = 9.8f;
+	const float jumpSpeed = 3;
+	private float vSpeed = 0; //Vertical speed
+
+
+	// Use this for initialization
+	void Start () {
+		controller = this.GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Calculate Velocity
+		Vector3 velocity = transform.right * Input.GetAxis ("Horizontal") * speed;
+		//Gravity and Jumping
+		if (controller.isGrounded) {
+			vSpeed = 0;
+		
+			if (Input.GetAxis ("Jump") == 1) {
+				vSpeed = jumpSpeed;
+				Debug.Log ("JUMP");
+			}
+		}
+		vSpeed -= gravity * Time.deltaTime;
+		velocity.y = vSpeed;
+		//Move Player
+		controller.Move (velocity * Time.deltaTime);
+
+
+	}
+}
