2176e539e98e01764de901026b2b7437e8f6f40d
Thomas Bassett <thomasbassett@hotmail.co.uk>
2015-11-14 01:29:48 +0000

    Added Models after fuck up
    Signed-off-by: Thomas Bassett <thomasbassett@hotmail.co.uk>

---
+++ Assets/Scenes/WorldGen/roomData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class roomData : MonoBehaviour {
+
+	public int size = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
